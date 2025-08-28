f2a8e67952603252023a4c7fa43a87f2cceede9d
laurenseperac-blip <laurenseperac@gmail.com>
2025-08-28 09:43:06 -0400

    Update FailExample.java

--- src/project/annotations/FailExample.java
+++ src/project/annotations/FailExample.java
@@ -1,7 +1,7 @@
 package project.annotations;
 
 public class FailExample {
-	public static void main(String[] args){
-	    System.out.println("Hello World")
-}
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
