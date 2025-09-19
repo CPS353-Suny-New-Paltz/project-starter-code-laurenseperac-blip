6e8baf8551ea6aba9bc52d7ea57c8d9aaee3cd82
laurenseperac-blip <laurenseperac@gmail.com>
2025-09-19 18:01:34 -0400

    fix conceptual api to be client prototype

--- src/conceptual/ComputeEngineAPI.java
+++ src/conceptual/ComputeEngineAPI.java
@@ -1,5 +1,8 @@
 package conceptual;
 
+import project.annotations.ConceptualAPI;
+
+@ConceptualAPI
 public interface ComputeEngineAPI {
 	ComputeResult performComputation(ComputeRequest request);
 }
--- src/conceptual/ComputeEngineAPIPrototype.java
+++ src/conceptual/ComputeEngineAPIPrototype.java
@@ -1,47 +1,20 @@
 package conceptual;
 
-public class ComputeEngineAPIPrototype implements ComputeEngineAPI {
-	@Override
-	@ConceptualAPIPrototype
-	public ComputeResult performComputation(ComputeRequest request) {
-		int input = request.getInput();
-		int output = largestPrimeSmallerThan(input);
-		
-		return new ComputeResult() {
+public class ComputeEngineAPIPrototype {
+	
+	public void prototype(ComputeEngineAPI engine) {
+		ComputeRequest request = new ComputeRequest() {
 			@Override
-			public int getOutput() {
-				return output;
+			public int getInput() {
+				return 100;
 			}
+		
 		};
+		
+		ComputeResult result = engine.performComputation(request);
+		
+		System.out.println("Largest prime smaller than input: " + result.getOutput());
+		
 	}
 	
-	private int largestPrimeSmallerThan(int n) {
-		if (n <= 2) { 
-			return -1;
-		}
-		for (int candidate = n -1; candidate > 1; candidate--) {
-			if (isPrime(candidate)) {
-				return candidate; 
-			}
-		}
-		return -1;
-	}
-	
-	private boolean isPrime(int x) {
-		if (x <= 1) {
-			return false;
-		}
-		if (x == 2) {
-			return true;
-		}
-		if (x % 2 == 0) {
-			return false;
-		}
-		for (int i = 3; i * i <= x; i += 2) {
-			if (x % i == 0) {
-				return false;
-			}
-		}
-		return true;
-	}
 }
