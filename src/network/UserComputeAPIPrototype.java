9ff682c3bf3d4e6d35d0fc3316122b58df0acc38
laurenseperac-blip <laurenseperac@gmail.com>
2025-09-19 18:19:25 -0400

    fix braces

--- src/network/UserComputeAPIPrototype.java
+++ src/network/UserComputeAPIPrototype.java
@@ -4,9 +4,15 @@ public class UserComputeAPIPrototype {
 	
 	public void prototype(UserComputeAPI api) {
 		JobRequest request = new JobRequest() {
-			public String getInputSource() { return "input.txt"; }
-			public String getOutputDestination() { return "output.txt"; }
-			public String getDelimiter() { return ":"; }
+			public String getInputSource() { 
+				return "input.txt"; 
+			}
+			public String getOutputDestination() { 
+				return "output.txt"; 
+			}
+			public String getDelimiter() { 
+				return ":"; 
+			}
 		};
 		
 		JobResponse response = api.submitJob(request);
