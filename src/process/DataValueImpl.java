fdec3989900bb51f592d9f85692e68bbaa0beacd
laurenseperac-blip <laurenseperac@gmail.com>
2025-09-22 14:16:53 -0400

    Add DataValue Implementation

---
+++ src/process/DataValueImpl.java
@@ -0,0 +1,14 @@
+package process;
+
+public class DataValueImpl implements DataValue{
+	private final int value;
+	
+	public DataValueImpl(int value) {
+		this.value = value;
+	}
+	
+	@Override
+	public int getValue() {
+		return value;
+	}
+}
