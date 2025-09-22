0177387ea6485b13a808f8f1b96ae33f39d1c3fa
laurenseperac-blip <laurenseperac@gmail.com>
2025-09-22 14:07:56 -0400

    Add JobResponse Implementation

---
+++ src/network/JobResponseImpl.java
@@ -0,0 +1,22 @@
+package network;
+
+public class JobResponseImpl implements JobResponse {
+	private final boolean success;
+	private final String message;
+	
+	public JobResponseImpl(boolean success, String message) {
+		this.success = success;
+		this.message = message;
+	}
+	
+	@Override
+	public boolean isSuccess() {
+		return success;
+	}
+	
+	@Override
+	public String getMessage() {
+		return message;
+	}
+
+}
