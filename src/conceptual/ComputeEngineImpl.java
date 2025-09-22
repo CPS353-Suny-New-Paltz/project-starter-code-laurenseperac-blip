1dd5c4aa91e7302e111ab4e60fd64656e35c4a64
laurenseperac-blip <laurenseperac@gmail.com>
2025-09-22 14:06:13 -0400

    Add ComputeEngine Implementation and ComputeResult Implementation (#13)
    * implementation of network api
    
    * fix braces
    
    * implement process api
    
    * add conceptual api
    
    * fix indentation
    
    * fixed indentation again
    
    * delete incorrect annotations
    
    * fix conceptual api to be client prototype
    
    * fix network to be a prototype of the client
    
    * fix process to be a prototype of the client of the api
    
    * fix braces
    
    * Add ComputeEngine Implementation
    
    * Create ComputeResult Implementation

---
+++ src/conceptual/ComputeEngineImpl.java
@@ -0,0 +1,19 @@
+package conceptual;
+
+import project.annotations.ConceptualAPI;
+
+@ConceptualAPI
+public class ComputeEngineImpl implements ComputeEngineAPI {
+	
+	private final ComputeEngineAPI computeEngine;
+	
+	public ComputeEngineImpl(ComputeEngineAPI computeEngine) {
+		this.computeEngine = computeEngine;
+	}
+	
+	@Override
+	public ComputeResult performComputation(ComputeRequest request) {
+		return new ComputeResultImpl(-1); // unimplemented
+
+	}
+}
