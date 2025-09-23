4e21664eb66e9e942010078f7cb1b923b734335e
laurenseperac-blip <laurenseperac@gmail.com>
2025-09-23 14:37:56 -0400

    add network api impl

---
+++ src/network/UserComputeImpl.java
@@ -0,0 +1,17 @@
+package network;
+
+import project.annotations.NetworkAPI;
+
+@NetworkAPI
+public class UserComputeImpl implements UserComputeAPI{
+	private final UserComputeAPI userCompute;
+
+	public UserComputeImpl(UserComputeAPI userCompute) {
+		this.userCompute = userCompute;
+	}
+
+	@Override
+	public JobResponse submitJob(JobRequest request) {
+		return new JobResponseImpl(false, "unimplemented");
+	}
+}
