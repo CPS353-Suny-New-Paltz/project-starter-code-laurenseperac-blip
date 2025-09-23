edf278ec5840e79544c8be5c00a990c97d2552b4
laurenseperac-blip <laurenseperac@gmail.com>
2025-09-23 17:17:12 -0400

    Create TestStorageCompute.java

---
+++ test/process/TestStorageCompute.java
@@ -0,0 +1,25 @@
+package process;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@Tag("skip")
+public class TestStorageCompute {
+
+	@Test
+	void testReadWrite() {
+		StorageComputeAPI storage = new StorageComputeImpl(null);
+		DataValue input = storage.readInput("inputKey");
+
+		assertNotNull(input);
+		assertEquals(-1, input.getValue());
+
+		boolean success = storage.writeOutput("outputKey", input);
+		assertTrue(success);
+	}
+
+}
