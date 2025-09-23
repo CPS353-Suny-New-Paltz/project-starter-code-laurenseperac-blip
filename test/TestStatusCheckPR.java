f93d4cf61d704e854fd94a69c129953b480fa047
laurenseperac-blip <laurenseperac@gmail.com>
2025-09-23 14:04:34 -0400

    comment out testpr (#17)
    * fix annotations
    
    * comment out testPR
    
    * comment out @test
    
    * fix typos

--- test/TestStatusCheckPR.java
+++ test/TestStatusCheckPR.java
@@ -26,8 +26,8 @@ public class TestStatusCheckPR {
     private static final String SUCCESS = "success";
     private static final String APPROVED = "APPROVED";
     
-    @Test
-  /*  public void testPullRequest() throws Exception {
+   // @Test
+    public void testPullRequest() throws Exception {
         String baseApiPath = getBaseApiPath();
         String toCurl = baseApiPath + "pulls?state=all";
         String pullRequests = curl(toCurl);
@@ -44,7 +44,7 @@ public class TestStatusCheckPR {
             }
         }
         Assertions.assertTrue(foundPullRequest, "No pull request with required status checks (failure, then success) and reviewer approval found");
-    } */
+    } 
     
     // query the git remote to find the repo URL
     private String getBaseApiPath() throws Exception {
