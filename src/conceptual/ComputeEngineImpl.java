2277c69ab6e9a45f7a716e271f63d85116cfba66
laurenseperac-blip <laurenseperac@gmail.com>
2025-09-23 14:32:39 -0400

    add conceptual api impl (#22)

--- src/conceptual/ComputeEngineImpl.java
+++ src/conceptual/ComputeEngineImpl.java
@@ -4,13 +4,13 @@ import project.annotations.ConceptualAPI;
 
 @ConceptualAPI
 public class ComputeEngineImpl implements ComputeEngineAPI {
-	
+
 	private final ComputeEngineAPI computeEngine;
-	
+
 	public ComputeEngineImpl(ComputeEngineAPI computeEngine) {
 		this.computeEngine = computeEngine;
 	}
-	
+
 	@Override
 	public ComputeResult performComputation(ComputeRequest request) {
 		return new ComputeResultImpl(-1); // unimplemented
--- src/conceptual/ComputeResultImpl.java
+++ src/conceptual/ComputeResultImpl.java
@@ -2,11 +2,11 @@ package conceptual;
 
 public class ComputeResultImpl implements ComputeResult {
 	private final int output;
-	
+
 	public ComputeResultImpl(int output) {
 		this.output = output;
 	}
-	
+
 	@Override
 	public int getOutput() {
 		return output;
