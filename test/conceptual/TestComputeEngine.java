6fb2e03c7c199c9bd89fce829cd257d49080e207
laurenseperac-blip <laurenseperac@gmail.com>
2025-09-23 16:58:42 -0400

    Add compute engine test (#26)
    * create TestComputeEngine
    
    * change test value to fail
    
    * tag to filter
    
    * fix import syntax
    
    * fix imports again

---
+++ test/conceptual/TestComputeEngine.java
@@ -0,0 +1,25 @@
+package conceptual;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@Tag("skip")
+public class TestComputeEngine {
+
+	@Test
+	void testPerformComputationWithMock() {
+		ComputeRequest mockRequest = mock(ComputeRequest.class);
+		when(mockRequest.getInput()).thenReturn(5);
+
+		ComputeEngineAPI engine = new ComputeEngineImpl(null);
+		ComputeResult result = engine.performComputation(mockRequest);
+
+		assertNotNull(result);
+		assertEquals(3, result.getOutput());
+	}
+
+}
