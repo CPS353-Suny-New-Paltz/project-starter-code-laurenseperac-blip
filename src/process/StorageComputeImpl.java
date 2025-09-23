6a70b1eec66e9beb361fb6f8b5c8ed3cb84d36c6
laurenseperac-blip <laurenseperac@gmail.com>
2025-09-23 14:41:29 -0400

    add process api impl

---
+++ src/process/StorageComputeImpl.java
@@ -0,0 +1,21 @@
+package process;
+
+public class StorageComputeImpl implements StorageComputeAPI {
+
+	private final StorageComputeAPI storageCompute;
+
+	public StorageComputeImpl(StorageComputeAPI storageCompute) {
+		this.storageCompute = storageCompute;
+	}
+
+	@Override
+	public DataValue readInput(String key) {
+		return new DataValueImpl(-1); //unimplemented
+	}
+
+	@Override
+	public boolean writeOutput(String key, DataValue value) {
+		return false;
+	}
+
+}
