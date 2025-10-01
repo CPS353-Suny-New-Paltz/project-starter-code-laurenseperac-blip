e9326b75b5b00a192fa4a699affa100fb6bf12c6
laurenseperac-blip <laurenseperac@gmail.com>
2025-10-01 13:16:10 -0400

    test storage comput implememtation

--- test/process/TestStorageCompute.java
+++ test/process/TestStorageCompute.java
@@ -2,8 +2,15 @@ package process;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -11,15 +18,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestStorageCompute {
 
 	@Test
-	void testReadWrite() {
+	void testReadWrite(@TempDir File tempDir) throws IOException {
+		File inputFile = new File(tempDir, "input.txt");
+		try (FileWriter writer = new FileWriter(inputFile)) {
+			writer.write("-1");
+		}
+		
 		StorageComputeAPI storage = new StorageComputeImpl();
-		DataValue input = storage.readInput("inputKey");
-
+		
+		// Test read
+		DataValue input = storage.readInput(inputFile.getAbsolutePath());
 		assertNotNull(input);
 		assertEquals(-1, input.getValue());
-
-		boolean success = storage.writeOutput("outputKey", input);
+		
+		//Test write
+		File outputFile = new File(tempDir, "output.txt");
+		boolean success = storage.writeOutput(outputFile.getAbsolutePath(), input);
 		assertTrue(success);
+		
+		DataValue output = storage.readInput(outputFile.getAbsolutePath());
+		assertEquals(-1, output.getValue());
 	}
 
 }
