2bf7484e6298881d231f864ae592d5d35a003db6
laurenseperac-blip <laurenseperac@gmail.com>
2025-09-30 21:55:43 -0400

    implement user compute

--- src/network/UserComputeImpl.java
+++ src/network/UserComputeImpl.java
@@ -1,18 +1,62 @@
 package network;
 
+import conceptual.ComputeEngineAPI;
+import conceptual.ComputeRequest;
+import conceptual.ComputeResult;
+import process.DataValue;
+import process.DataValueImpl;
+import process.StorageComputeAPI;
 import project.annotations.NetworkAPI;
+
 
 @NetworkAPI
 public class UserComputeImpl implements UserComputeAPI{
-	private final UserComputeAPI userCompute;
+	private final ComputeEngineAPI engine;
+	private final StorageComputeAPI storage;
 
-	public UserComputeImpl(UserComputeAPI userCompute) {
-		this.userCompute = userCompute;
+	public UserComputeImpl(ComputeEngineAPI engine, StorageComputeAPI storage) {
+		this.engine = engine;
+		this.storage = storage;
 	}
 
 	@Override
 	public JobResponse submitJob(JobRequest request) {
-		// test expects "implemented" for test
-		return new JobResponseImpl(false, "implemented");
+		try {
+			//reads ints from storage
+			DataValue inputVal = storage.readInput(request.getInputSource());
+			
+			//pass to compute engine
+			ComputeRequest compReq = () -> inputVal.getValue();
+			ComputeResult compRes = engine.performComputation(compReq);
+			
+			//write to storage
+			DataValue outputVal = new DataValueImpl(compRes.getOutput());
+			storage.writeOutput(request.getOutputDestination(), outputVal);
+			
+			//return status
+			return new JobResponse() {
+				@Override
+				public boolean isSuccess() {
+					return true;
+				}
+				@Override
+				public String getMessage() {
+					return "Job completed successfully";
+				}
+			};
+		} catch (Exception e) {
+			return new JobResponse() {
+				
+				@Override
+				public boolean isSuccess() {
+					return false;
+				}
+				
+				@Override
+				public String getMessage() {
+					return "Job failed";
+				}
+			};
+		}
 	}
 }
