5a7a0e7d9d8fbcd07013f98c05c77a44ba518ab6
laurenseperac-blip <laurenseperac@gmail.com>
2025-10-01 12:22:42 -0400

    remove filter from test

--- test/conceptual/TestComputeEngine.java
+++ test/conceptual/TestComputeEngine.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@Tag("skip")
 public class TestComputeEngine {
 
 	@Test
