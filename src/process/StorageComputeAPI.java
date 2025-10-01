f703ec192e07ed11c9afcc83b6d9143b7bad09c2
laurenseperac-blip <laurenseperac@gmail.com>
2025-10-01 12:24:14 -0400

    Implementation tests, UserCompute and ComputeEngine (#39)
    * test user compute implementation
    
    * remove filter from test

--- src/process/StorageComputeAPI.java
+++ src/process/StorageComputeAPI.java
@@ -4,6 +4,6 @@ import project.annotations.ProcessAPI;
 
 @ProcessAPI
 public interface StorageComputeAPI {
-	DataValue readInput(String source);
-	boolean writeOutput(String destination, DataValue data);
+	DataValue readInput(String filePath);
+	boolean writeOutput(String filePath, DataValue value);
 }
