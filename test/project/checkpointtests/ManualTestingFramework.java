3bb9a16d1facb69d7cd6774063b166370945dfaa
laurenseperac-blip <laurenseperac@gmail.com>
2025-10-02 10:14:56 -0400

    create manual testing framework

---
+++ test/project/checkpointtests/ManualTestingFramework.java
@@ -0,0 +1,24 @@
+package project.checkpointtests;
+
+public class ManualTestingFramework {
+    
+    public static final String INPUT = "manualTestInput.txt";
+    public static final String OUTPUT = "manualTestOutput.txt";
+
+    public static void main(String[] args) {
+        // TODO 1:
+        // Instantiate a real (ie, class definition lives in the src/ folder) implementation 
+        // of all 3 APIs
+        //
+   
+    	
+        // TODO 2:
+        // Run a computation with an input file of <root project dir>/manualTestInput.txt
+        // and an output of <root project dir>/manualTestOutput.txt, with a delimiter of ',' 
+        //
+        //
+        // Helpful hint: the working directory of this program is <root project dir>,
+        // so you can refer to the files just using the INPUT/OUTPUT constants. You do not 
+        // need to (and should not) actually create those files in your repo
+    }
+}
