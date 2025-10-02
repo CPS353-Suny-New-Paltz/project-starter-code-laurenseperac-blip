747bbe1a317c40b4dec1ea68a45758c65a255594
laurenseperac-blip <laurenseperac@gmail.com>
2025-10-02 10:59:06 -0400

    fix manual testing framework

--- test/project/checkpointtests/ManualTestingFramework.java
+++ test/project/checkpointtests/ManualTestingFramework.java
@@ -12,37 +12,19 @@ import process.StorageComputeImpl;
 import process.DataValue;
 import process.DataValueImpl;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class ManualTestingFramework {
     
     public static final String INPUT = "manualTestInput.txt";
     public static final String OUTPUT = "manualTestOutput.txt";
 
     public static void main(String[] args) {
-        // TODO 1:
-        // Instantiate a real (ie, class definition lives in the src/ folder) implementation 
-        // of all 3 APIs
-        //
-    	   // TODO 2:
-        // Run a computation with an input file of <root project dir>/manualTestInput.txt
-        // and an output of <root project dir>/manualTestOutput.txt, with a delimiter of ',' 
-        //
-        //
-        // Helpful hint: the working directory of this program is <root project dir>,
-        // so you can refer to the files just using the INPUT/OUTPUT constants. You do not 
-        // need to (and should not) actually create those files in your repo
-    	
-    	try {
+        try {
             ComputeEngineAPI engine = new ComputeEngineImpl(null);
             StorageComputeAPI storage = new StorageComputeImpl();
             UserComputeAPI user = new UserComputeImpl(engine, storage);
 
-            List<String> results = new ArrayList<>();
-
             while (true) {
-            	DataValue inputVal;
+                DataValue inputVal;
                 try {
                     inputVal = storage.readInput(INPUT);
                 } catch (RuntimeException e) {
@@ -53,18 +35,13 @@ public class ManualTestingFramework {
                 ComputeRequest request = new ComputeRequestImpl(inputVal.getValue());
                 ComputeResult result = engine.performComputation(request);
 
-                // Write the output immediately for each input
                 storage.writeOutput(OUTPUT, new DataValueImpl(result.getOutput()));
             }
-
-            String joined = String.join(",", results);
-            storage.writeOutput(OUTPUT, new DataValueImpl(joined.hashCode())); 
 
             System.out.println("Computation finished. Results written to " + OUTPUT);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-     
     }
 }
