03ff9f919369680b3f3295914d879100acb54cbf
laurenseperac-blip <laurenseperac@gmail.com>
2025-10-08 12:17:57 -0400

    Update ManualTestingFramework.java

--- test/project/checkpointtests/ManualTestingFramework.java
+++ test/project/checkpointtests/ManualTestingFramework.java
@@ -1,35 +1,39 @@
 package project.checkpointtests;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import conceptual.ComputeEngineAPI;
 import conceptual.ComputeEngineImpl;
-import conceptual.ComputeRequest;
-import conceptual.ComputeRequestImpl;
-import conceptual.ComputeResult;
 import process.StorageComputeAPI;
 import process.StorageComputeImpl;
+import network.UserComputeAPI;
 import network.UserComputeImpl;
-import network.JobResponse;
 import network.JobRequest;
 import network.JobRequestImpl;
-import network.JobResponseImpl;
-import network.UserComputeAPI;
-
+import network.JobResponse;
 
 public class ManualTestingFramework {
-	
-	public static final String INPUT = "numbers.txt";
-	public static final String OUTPUT = "output.txt";
-	
-	public static void main(String[] args) {
-		StorageComputeAPI storage = new StorageComputeImpl();
-		ComputeEngineAPI engine = new ComputeEngineImpl();
-		UserComputeAPI userAPI = new UserComputeImpl(engine, storage);
-	    
-		JobRequest job = new JobRequestImpl(INPUT, OUTPUT, ",");
-		JobResponse response = userAPI.submitJob(job);
-		
-		System.out.println("Job success: " + response.isSuccess());
-		System.out.println("Message: " + response.getMessage());
 
-	}
+    public static final String INPUT = "numbers.txt";
+    public static final String OUTPUT = "output.txt";
+
+    public static void main(String[] args) throws Exception {
+        Path inputPath = Paths.get(INPUT);
+        if (inputPath.getParent() != null) {
+            Files.createDirectories(inputPath.getParent());
+        }
+        Files.write(inputPath, "1\n2\n3".getBytes());
+
+        StorageComputeAPI storage = new StorageComputeImpl();
+        ComputeEngineAPI engine = new ComputeEngineImpl();
+        UserComputeAPI userAPI = new UserComputeImpl(engine, storage);
+
+        JobRequest job = new JobRequestImpl(INPUT, OUTPUT, ",");
+        JobResponse response = userAPI.submitJob(job);
+
+        System.out.println("Job success: " + response.isSuccess());
+        System.out.println("Message: " + response.getMessage());
+    }
 }
