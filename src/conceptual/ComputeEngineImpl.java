f2e4b0775a9c5cd723ec046321f2ddfe0da5796d
laurenseperac-blip <laurenseperac@gmail.com>
2025-11-27 14:17:03 -0500

    fix braces for checksytle

--- src/conceptual/ComputeEngineImpl.java
+++ src/conceptual/ComputeEngineImpl.java
@@ -70,12 +70,18 @@ public class ComputeEngineImpl implements ComputeEngineAPI {
     }
 
     private boolean isPrime(int x) {
-        if (x < 2) return false;
-        if (x % 2 == 0) return x == 2;
+        if (x < 2) {
+        	return false;
+        }
+        if (x % 2 == 0) {
+        	return x == 2;
+        }
 
         int r = (int) Math.sqrt(x);
         for (int i = 3; i <= r; i += 2) {
-            if (x % i == 0) return false;
+            if (x % i == 0) {
+            	return false;
+            }
         }
         return true;
     }
