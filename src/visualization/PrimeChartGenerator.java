cb6abc456b5f85b5212e662cd276c51f1f11c15a
laurenseperac-blip <laurenseperac@gmail.com>
2025-12-07 15:27:57 -0500

    fix braces

--- src/visualization/PrimeChartGenerator.java
+++ src/visualization/PrimeChartGenerator.java
@@ -21,7 +21,9 @@ public class PrimeChartGenerator {
         int nonPrimeCount = 0;
 
         for (int num : numbers) {
-            if (num < 0) continue;
+            if (num < 0) {
+            	continue;
+            }
 
             if (isPrime(num)) {
                 primeCount++;
@@ -58,12 +60,20 @@ public class PrimeChartGenerator {
     }
 
     private static boolean isPrime(int n) {
-        if (n < 2) return false;
-        if (n == 2) return true;
-        if (n % 2 == 0) return false;
+        if (n < 2) {
+        	return false;
+        }
+        if (n == 2) {
+        	return true;
+        }
+        if (n % 2 == 0) {
+        	return false;
+        }
         int sqrt = (int) Math.sqrt(n);
         for (int i = 3; i <= sqrt; i += 2) {
-            if (n % i == 0) return false;
+            if (n % i == 0) {
+            	return false;
+            }
         }
         return true;
     }
